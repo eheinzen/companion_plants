fd9e5d2649424ba77344110b59840482c0b124cd
Ethan Heinzen <heinzen.ethan@mayo.edu>
2022-09-07 10:30:05 -0500

    give two pages

--- companions.R
+++ companions.R
@@ -8,26 +8,26 @@ CONSIDER <- c("apple", "basil", "bush beans", "carrots", "chives",
 
 dat <- read_csv("companions.csv", col_types = cols()) %>%
   filter(!duplicated(map2(first, second, ~ paste(sort(c(.x, .y)), collapse = "---")))) %>%
-  filter(first %in% CONSIDER, second %in% CONSIDER) %>%
   mutate(direction = if_else(direction == 1, "u", "d"))
 
-nodes <- dat %>%
-  pivot_longer(c(first, second), values_to = "node") %>%
-  count(node)
+generate <- function(links_file, nodes_file, dat) {
+  nodes <- dat %>%
+    pivot_longer(c(first, second), values_to = "node") %>%
+    count(node)
 
+  glue::glue('{"source": "<<dat$first>>", "target": "<<dat$second>>", "direction": "<<dat$direction>>"}', .open = "<<", .close = ">>") %>%
+    paste0(collapse = ",\n") %>%
+    paste0("links = [\n", ., "\n];") %>%
+    cat(file = links_file)
 
-glue::glue('{"source": "<<dat$first>>", "target": "<<dat$second>>", "direction": "<<dat$direction>>"}', .open = "<<", .close = ">>") %>%
-  paste0(collapse = ",\n") %>%
-  paste0("links = [\n", ., "\n];") %>%
-  cat(file = "links.js")
+  glue::glue('"<<nodes$node>>": {"name": "<<nodes$node>>", "size": <<nodes$n>>}', .open = "<<", .close = ">>") %>%
+    paste0(collapse = ",\n") %>%
+    paste0("nodes = {\n", ., "\n};") %>%
+    cat(file = nodes_file)
+}
 
-glue::glue('"<<nodes$node>>": {"name": "<<nodes$node>>", "size": <<nodes$n>>}', .open = "<<", .close = ">>") %>%
-  paste0(collapse = ",\n") %>%
-  paste0("nodes = {\n", ., "\n};") %>%
-  cat(file = "nodes.js")
-
-
-
+generate("links_full.js", "nodes_full.js", dat)
+generate("links.js", "nodes.js", filter(dat, first %in% CONSIDER, second %in% CONSIDER))
 
 
 
